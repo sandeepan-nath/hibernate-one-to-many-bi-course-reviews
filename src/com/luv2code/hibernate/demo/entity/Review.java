539fc97e1d69b28ddd23afbbd7ea3339b2801cd2
Sandeepan <sandeepan.nath@payufin.com>
2020-06-21 15:47:15 +0530

    src files

---
+++ src/com/luv2code/hibernate/demo/entity/Review.java
@@ -0,0 +1,84 @@
+package com.luv2code.hibernate.demo.entity;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="review")
+public class Review {
+
+	// define fields
+	
+	// define constructors
+	
+	// define getter/setters
+	
+	// define tostring
+	
+	// annotate fields
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="comment")
+	private String comment;
+	
+	@ManyToOne(cascade= {CascadeType.PERSIST, CascadeType.MERGE,
+			 CascadeType.DETACH, CascadeType.REFRESH})
+	@JoinColumn(name="course_id")
+	private Course course;
+	
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+
+	public Review() {
+		
+	}
+
+	public Review(String comment) {
+		this.comment = comment;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	@Override
+	public String toString() {
+		return "Review [id=" + id + ", comment=" + comment + "]";
+	}
+	
+}
+
+
+
+
+
